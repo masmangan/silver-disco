d18cb5fb29577c5147408b407c325a53ed75bd22
Marco Aurelio Souza Mangan <10067320@facin.portoalegre.pucrsnet.br>
2018-11-07 20:55:07 -0200

    #3

--- silver-disco.cpp
+++ silver-disco.cpp
@@ -3,13 +3,41 @@
 // marco.mangan@pucrs.br
 
 #include <iostream>
-
-
+#include <string>
 using namespace std;
+
+namespace silver
+{
+	class candidato
+	{
+		private:
+			string nome;
+			int numero;
+		public:
+			candidato(string nome, int numero);
+			friend ostream &operator<<(ostream&, const candidato&);
+
+	};
+
+	candidato::candidato(string nome, int numero) {
+		this->nome = nome;
+		this->numero = numero;
+	}
+
+	ostream &operator<<(ostream& os, const candidato& cand) {
+		os << cand.nome << " " << cand.numero;
+		return os;
+	}
+
+}
+
 
 int main()
 {
+	silver::candidato samba("Samba", 91);
+
 	cout << "Sistema Eletrônico de Votação" << endl;
+	cout << samba << endl;
 	return 0;
 }
 
